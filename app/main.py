038bb297cd5bdf139f9d884f677bb976b5b7e1c8
gryffindoraf <phynew0111@gmail.com>
2024-05-02 22:09:52 -0400

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -4,12 +4,13 @@ import socket
 def main():
     print("Logs from your program will appear here!")
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_socket, _ = server_socket.accept()
-    print("Connected by", client_socket.getpeername())
+
     while True:
+        client_socket, _ = server_socket.accept()
+        print("Connected by", client_socket.getpeername())
         response = "+PONG\r\n"
-        client_socket.sendall(response.encode())
         data = client_socket.recv(1024).decode().strip()
+        client_socket.sendall(response.encode())
         if not data:
             break
 
